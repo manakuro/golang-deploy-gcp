789ca0540515f17634fe7be7311bb4769d38cb06
manato.kuroda <a7108100@gmail.com>
2019-11-23 17:21:02 +0900

    chore: change responding text

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello")
+		fmt.Fprintf(w, "Hello Go App!")
 	})
 
 	http.ListenAndServe(":8080", nil)
