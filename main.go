1c334f49b57b3bd42891d90e7df36420b51f6233
manato.kuroda <a7108100@gmail.com>
2019-11-29 10:38:35 +0900

    feat: add text

--- main.go
+++ main.go
@@ -7,7 +7,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello Go App!")
+		fmt.Fprintf(w, "Hello Go App! This is my first deploy!")
 	})
 
 	http.ListenAndServe(":8080", nil)
